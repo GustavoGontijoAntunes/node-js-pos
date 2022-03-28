1682a27df1933c6a8a27d926d4e7ea192edc6ce4
Gustavo Gontijo Antunes <51254362+GustavoGontijoAntunes@users.noreply.github.com>
2022-03-27 22:26:27 -0300

    Add files via upload
    Api de Segurança

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,22 @@
+const jwt = require ('jsonwebtoken')
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/products.js')
+const controller = require('../controllers/products.js');
+const utilSec = require('../utils/utilSec');
 
-router.get('/', controller.getProduct);
-router.get('/:id', controller.getProductById);
-router.post('/', controller.createProduct);
-router.delete('/:id', controller.deleteProduct);
-router.patch('/:id', controller.updateProduct);
+const knex = require('knex')({
+    client: 'pg',
+    debug: true,
+    connection: {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+    }
+});
+
+router.get('/', utilSec.checkToken, controller.getProduct);
+router.get('/:id', utilSec.checkToken, controller.getProductById);
+router.post('/', utilSec.checkToken, utilSec.isAdmin, controller.createProduct);
+router.delete('/:id', utilSec.checkToken, utilSec.isAdmin, controller.deleteProduct);
+router.patch('/:id', utilSec.checkToken, utilSec.isAdmin, controller.updateProduct);
 
 module.exports = router;
